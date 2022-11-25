7e81fd24af62f52abb58c1402a315dc3273fa925
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-11-25 11:58:50 +0400

    feat: Implement days in month func

--- cmd/ge-tax-calc/main.go
+++ cmd/ge-tax-calc/main.go
@@ -130,5 +130,9 @@ func getCurrencyRatesByDate(date time.Time, codes ...currCode) (Rates, error) {
 	return resp, nil
 }
 
-	fmt.Println(string(b))
+// The reason it works is that we generate a date one month on from the target one (m+1), but set the day of month to 0.
+// Days are 1-indexed, so this has the effect of rolling back one day to the last day of the previous month
+// (our target month of m). Calling Day() then procures the number we want.
+func daysIn(m time.Month, year int) int {
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
 }
