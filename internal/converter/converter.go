0e144a34b02126f628bd810e9ede508548296e29
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-11-27 01:00:01 +0400

    feat: Implement common converter

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -13,14 +13,20 @@ import (
 	"github.com/obalunenko/georgia-tax-calculator/pkg/nbggovge/option"
 )
 
+// ErrCurrencyNotSet returned when currency code for conversion not set.
+var ErrCurrencyNotSet = errors.New("currency not set")
+
+// Converter is a converter of money from one currency to another.
 type Converter struct {
 	client nbggovge.Client
 }
 
+// NewConverter constructor.
 func NewConverter(client nbggovge.Client) *Converter {
 	return &Converter{client: client}
 }
 
+// Response of conversion.
 type Response struct {
 	Amount   float64
 	Currency string
@@ -30,36 +36,82 @@ func (r Response) String() string {
 	return fmt.Sprintf("%.2f %s", r.Amount, r.Currency)
 }
 
+// ConvertToGel shortcut for Convert to GEL.
 func (c Converter) ConvertToGel(ctx context.Context, amount float64, from string, date time.Time) (Response, error) {
-	rates, err := c.client.Rates(ctx, option.WithDate(date), option.WithCurrency(from))
+	return c.Convert(ctx, amount, from, currencies.GEL, date)
+}
+
+// Convert converts amount from currency to by rates according to passed date.
+func (c Converter) Convert(ctx context.Context, amount float64, from, to string, date time.Time) (Response, error) {
+	if from == "" {
+		return Response{}, fmt.Errorf("from: %w", ErrCurrencyNotSet)
+	}
+
+	if to == "" {
+		return Response{}, fmt.Errorf("to: %w", ErrCurrencyNotSet)
+	}
+
+	rates, err := c.client.Rates(ctx, option.WithDate(date), option.WithCurrency(from), option.WithCurrency(to))
 	if err != nil {
 		return Response{}, err
 	}
 
-	currency, err := rates.CurrencyByCode(from)
+	fromCurrency, err := c.getCurrencyRates(from, rates)
 	if err != nil {
 		return Response{}, err
 	}
 
-	sum := convert(amount, currency.Rate)
+	toCurrency, err := c.getCurrencyRates(to, rates)
+	if err != nil {
+		return Response{}, err
+	}
+
+	fromingel := convert(decimal.NewFromFloat(amount), decimal.NewFromFloat(fromCurrency.Rate))
+
+	tosum := convert(fromingel, decimal.NewFromFloat(1/toCurrency.Rate))
 
 	return Response{
-		Amount:   sum,
-		Currency: currencies.GEL,
+		Amount:   round(tosum, 2),
+		Currency: to,
 	}, nil
 }
 
-func (c Converter) Convert(ctx context.Context, amount float64, from, to string, date time.Time) (Response, error) {
-	return Response{}, errors.New("not implemented")
+func (c Converter) getCurrencyRates(code string, rates nbggovge.RatesResponse) (nbggovge.Currency, error) {
+	var (
+		currency nbggovge.Currency
+		err      error
+	)
+
+	if code == currencies.GEL {
+		currency = nbggovge.Currency{
+			Code:          currencies.GEL,
+			Quantity:      1,
+			RateFormated:  "1",
+			DiffFormated:  "0",
+			Rate:          1,
+			Name:          "GEL",
+			Diff:          0,
+			Date:          rates[0].Date,
+			ValidFromDate: rates[0].Date,
+		}
+	} else {
+		currency, err = rates.CurrencyByCode(code)
+		if err != nil {
+			return nbggovge.Currency{}, err
+		}
+	}
+
+	return currency, nil
 }
 
-func convert(amount, rate float64) float64 {
-	ad := decimal.NewFromFloat(amount)
-	rd := decimal.NewFromFloat(rate)
+func convert(amount, rate decimal.Decimal) decimal.Decimal {
+	res := amount.Mul(rate)
 
-	res := ad.Mul(rd)
+	return res
+}
 
-	rounded := res.Round(2)
+func round(amount decimal.Decimal, places int32) float64 {
+	rounded := amount.Round(places)
 
 	return rounded.InexactFloat64()
 }
