1ce781c2e0b127d87fcec8f3c05a94c696cfc9d3
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-12-08 03:12:49 +0400

    Debug

--- scripts/tests/run.sh
+++ scripts/tests/run.sh
@@ -2,6 +2,12 @@
 
 set -eu pipefail
 
+go version
+
+go list -m
+
+cat /usr/bin/log_build.txt
+
 SCRIPT_NAME="$(basename "$0")"
 
 echo "${SCRIPT_NAME} is running... "
@@ -11,6 +17,6 @@ if command -v "gotestsum" &>/dev/null; then
   GOTEST="gotestsum --format pkgname-and-test-fails --"
 fi
 
-${GOTEST} -race ./...
+${GOTEST} -race $(go list -m)/...
 
 echo "${SCRIPT_NAME} done."
